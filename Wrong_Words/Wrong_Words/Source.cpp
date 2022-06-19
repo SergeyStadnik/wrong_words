eb29116ed2b354f7a298e13104fe17d1a68ba720
SergeyStadnik <107566678+SergeyStadnik@users.noreply.github.com>
2022-06-19 17:51:27 +0300

    Greate_Project
    Greate_Progect

---
+++ Wrong_Words/Wrong_Words/Source.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+#include<time.h>
+#include<stdlib.h>
+using namespace std;
+
+
+int main() {
+
+	setlocale(0, "");
+	srand(time(NULL));
+
+	
+
+
+	system("pause");
+	return 0;
+}
